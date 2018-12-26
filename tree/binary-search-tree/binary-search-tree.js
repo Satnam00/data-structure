f45439fe217f7777bd28e64aff6e88dfb2dd864b
Karim Mirazul <karimmirazul.git@gmail.com>
2018-12-26 14:48:28 +0530

    add binary tree image for redme file

---
+++ tree/binary-search-tree/binary-search-tree.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var BinarySearchTree = /** @class */ (function () {
+    function BinarySearchTree() {
+        console.log('HURRY !!');
+    }
+    return BinarySearchTree;
+}());
+exports.BinarySearchTree = BinarySearchTree;
+new BinarySearchTree();
