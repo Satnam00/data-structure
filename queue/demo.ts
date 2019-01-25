cd3c0d004b21fda6000eaf787ef08f8e11c7db94
Karim Mirazul <karimmirazul.git@gmail.com>
2019-01-25 15:41:00 +0530

    add console

--- queue/demo.ts
+++ queue/demo.ts
@@ -6,6 +6,12 @@ queueArray.push(20);
 queueArray.push(30);
 queueArray.push(40);
 
+console.log('queue : ', queueArray._store);
+
 queueArray.pop();
 
 console.log('queue : ', queueArray._store);
+
+// output :
+//     queue :  [ 10, 20, 30, 40 ]
+//     queue :  [ 20, 30, 40 ]
